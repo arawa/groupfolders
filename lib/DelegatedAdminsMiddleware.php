38c2c21df0fa71c506e9a9bfe8a7197881b2fa08
Baptiste Fotia <fotia.baptiste@hotmail.com>
2022-05-09 14:48:17 +0200

    fix(php): Remove the '@' of '@RequireGroupFolderAdmin'
    This commit resolve a hotfix and high security
    
    Rsolved : https://github.com/arawa/groupfolders/issues/1

--- lib/DelegatedAdminsMiddleware.php
+++ lib/DelegatedAdminsMiddleware.php
@@ -1,105 +1,103 @@
+
 <?php
+
+/**
+ * @author Cyrille Bollu <cyr.debian@bollu.be> for Arawa (https://www.arawa.fr/)
+ *
+ * GroupFolders
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
 
 namespace OCA\GroupFolders;
 
+use OCA\GroupFolders\Service\DelegationService;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\Http\Response;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Middleware;
-use OCP\IAppConfig;
-use OCP\IGroup;
-use OCP\IGroupManager;
+use OCP\AppFramework\Utility\IControllerMethodReflector;
 use OCP\IRequest;
-use OCP\IUserSession;
 use Psr\Log\LoggerInterface;
 
 class DelegatedAdminsMiddleware extends Middleware {
 
-    /** @var string */
-    private $appName;
+	/** @var IControllerMethodReflector */
+	private $reflector;
 
-    /** @var IAppConfig */
-    private $appConfig;
+	/** @var DelegationService */
+	private $delegationService;
 
-    /** @var IGroupManager */
-    private $groupManager;
+	/** @var LoggerInterface */
+	private $logger;
 
-    /** @var IRequest */
-    private $request;
+	/** @var IRequest */
+	private $request;
 
-    /** @var IUserSession */
-    private  $userSession;
+	/**
+	 *
+	 * @param IControllerMethodReflector $reflector
+	 * @param DelegationService $delegationService
+	 * @param IRequest $request
+	 * @param LoggerInterface $logger
+	 *
+	 */
+	public function __construct(IControllerMethodReflector $reflector,
+				DelegationService $delegationService,
+				IRequest $request,
+				LoggerInterface $logger) {
 
-    /**
-     *
-     * @param string $appName
-     * @param IAppConfig $appConfig
-     * @param IgroupManager $groupManager
-     * @param IUserSession $userSession
-     *
-     */
-    public function __construct(string $appName,
-        IAppConfig $appConfig,
-        IgroupManager $groupManager,
-        IRequest $request,
-        IUserSession $userSession
-        ) {
-            $this->appName = $appName;
-            $this->appConfig = $appConfig;
-            $this->groupManager = $groupManager;
-            $this->request = $request;
-            $this->userSession = $userSession;
-    }
+				$this->reflector = $reflector;
+				$this->delegationService = $delegationService;
+				$this->logger = $logger;
+				$this->request = $request;
+	}
 
-    /**
-     *
-     * {@inheritDoc}
-     * @see \OCP\AppFramework\Middleware::beforeController()
-     *
-     * Throws an error when the user is not allowed to use the app's APIs
-     *
-     */
-    public function beforeController($controller, $methodName) {
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \OCP\AppFramework\Middleware::beforeController()
+	 *
+	 * Throws an error when the user is not allowed to use the app's APIs
+	 *
+	 */
+	public function beforeController($controller, $methodName) {
+	if ($this->reflector->hasAnnotation('RequireGroupFolderAdmin')) {	
+			if(!$this->delegationService->isAdmin()) {
+				$this->logger->error('User is not member of a delegated admins group');
+				throw new \Exception('User is not member of a delegated admins group', Http::STATUS_FORBIDDEN);
+			}
+		}
+	}
 
-        // method 'aclMappingSearch' implements its own access control and method 'isAdmin' must be accesible by everyone
-        if ($methodName !== 'aclMappingSearch' && $methodName !== 'isAdmin') {
-            // Get allowed groups from app's config
-            $appConfigKeys = $this->appConfig->getValues($this->appName, false);
-            $delegatedAdmins = str_word_count($appConfigKeys['delegated-admins'], 1, '_-');
-
-            // Find out if user is member of any group(s) granted delegated admin rights
-            $userGroups = $this->groupManager->getUserGroups($this->userSession->getUser());
-            $result = array_intersect($delegatedAdmins, array_map(function(IGroup $group) {
-                return $group->getDisplayName();
-            }, $userGroups));
-
-            // Throw an error when user is not member of any such groups
-            if (count($result) === 0) {
-                \OC::$server->get(LoggerInterface::class)->error('User is not member of a delegated admins group');
-                throw new \Exception('User is not member of a delegated admins group', Http::STATUS_FORBIDDEN);
-            }
-        }
-
-    }
-
-    /**
-     *
-     * {@inheritDoc}
-     * @see \OCP\AppFramework\Middleware::afterException()
-     *
-     */
-    public function afterException($controller, $methodName, \Exception $exception): Response {
-        if (stripos($this->request->getHeader('Accept'),'html') === false) {
-            $response = new JSONResponse(
-                    ['message' => $exception->getMessage()],
-                    (int)$exception->getCode()
-                    );
-        } else {
-            $response = new TemplateResponse('core', '403', ['message' => $exception->getMessage()], 'guest');
-            $response->setStatus((int)$exception->getCode());
-        }
-
-        return $response;
-    }
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \OCP\AppFramework\Middleware::afterException()
+	 *
+	 */
+	public function afterException($controller, $methodName, \Exception $exception): Response {
+		if (stripos($this->request->getHeader('Accept'),'html') === false) {
+			$response = new JSONResponse(
+				['message' => $exception->getMessage()],
+				(int)$exception->getCode()
+			);
+		} else {
+			$response = new TemplateResponse('core', '403', ['message' => $exception->getMessage()], 'guest');
+			$response->setStatus((int)$exception->getCode());
+		}
+		return $response;
+	}
 }
